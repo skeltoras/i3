fe1ced05ace190d95c38ba2e061f483a7576992a
Skeltoras <git@skeltoras.de>
2014-06-17 15:47:27 +0200

    changed functionality for devise to use username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # when allowing distinct User records with, e.g., "username" and "UserName"...
+  where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+  
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  #attr_accessor :login
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
